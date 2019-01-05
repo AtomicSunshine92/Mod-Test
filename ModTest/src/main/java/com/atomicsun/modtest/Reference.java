7d8cba4438136d07aadcbfb1ce1af0882b62e354
AtomicSunshine92 <lukeob5@gmail.com>
2019-01-05 21:18:29 +0000

    First version commit

---
+++ ModTest/src/main/java/com/atomicsun/modtest/Reference.java
@@ -0,0 +1,8 @@
+package com.atomicsun.modtest;
+
+public class Reference {
+    public static final String MODID = "modtest";
+    public static final String NAME = "Mod Test";
+    public static final String VERSION = "1.0a";
+
+}
